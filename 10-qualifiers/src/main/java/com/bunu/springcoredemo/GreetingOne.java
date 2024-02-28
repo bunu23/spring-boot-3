67d7f3e19e8783873d6be20c42fd3582cd88492e
Bunu <bunubhattarai39@gmail.com>
2024-02-28 14:39:05 -0500

    spring core

--- 10-qualifiers/src/main/java/com/bunu/springcoredemo/GreetingOne.java
+++ 10-qualifiers/src/main/java/com/bunu/springcoredemo/GreetingOne.java
@@ -3,9 +3,10 @@ package com.bunu.springcoredemo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class GreetingOne implements Greeting{
+public class GreetingOne implements Greeting {
     @Override
     public String getGreeting() {
         return "Hello World";
+
     }
 }
